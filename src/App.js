386344914235054df729970994ff3e49e31fa53e
sanyaverma2001 <sanyaverma99789@gmail.com>
2021-06-22 20:59:01 +0530

    added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Homepage from "./Components/Homepage/Homepage";
 import {BrowserRouter as Router,Route,Link,Switch} from 'react-router-dom';
 import Explore from "./Components/Explore/Explore"
 import Opensource from "./Components/Fields/Open-source/Opensource"
+import Dsa from "./Components/Fields/DSA/Dsa"
+
+
 
 function App() {
   return (
@@ -14,6 +17,8 @@ function App() {
         <Route exact path='/Homepage' component={Homepage} />
         <Route exact path='/Explore' component={Explore} />
         <Route exact path='/Opensource' component={Opensource} />
+        <Route exact path='/Dsa' component={Dsa} />
+
       </Switch>
     </Router>
     </>

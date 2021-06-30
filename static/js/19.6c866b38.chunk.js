db432bc98b6aea5a1e134fcdaa8dcb480fe24bd0
vedant_jain03 <vedantjainben10@gmail.com>
2021-06-30 15:02:38 +0530

    Updates

---
+++ static/js/19.6c866b38.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonphash_hub=this.webpackJsonphash_hub||[]).push([[19],{82:function(e,a,t){"use strict";var s=t(33);Object.defineProperty(a,"__esModule",{value:!0}),a.default=void 0;var h=s(t(0)),u=(0,s(t(34)).default)(h.default.createElement("path",{transform:"scale(1.33, 1.33)",d:"M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"}),"StarRate");a.default=u}}]);
+//# sourceMappingURL=19.6c866b38.chunk.js.map

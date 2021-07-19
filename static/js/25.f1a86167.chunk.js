77b537faba5d766785697a358e074e26f922becf
vedant_jain03 <vedantjainben10@gmail.com>
2021-07-19 17:27:06 +0530

    Updates

---
+++ static/js/25.f1a86167.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonphash_hub=this.webpackJsonphash_hub||[]).push([[25],{113:function(e,a,t){"use strict";var s=t(14);Object.defineProperty(a,"__esModule",{value:!0}),a.default=void 0;var h=s(t(0)),u=(0,s(t(15)).default)(h.default.createElement("path",{transform:"scale(1.33, 1.33)",d:"M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"}),"StarRate");a.default=u}}]);
+//# sourceMappingURL=25.f1a86167.chunk.js.map
